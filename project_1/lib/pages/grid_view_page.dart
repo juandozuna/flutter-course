485e81505e860cd4ffac0c002ff0815ff9dbd80c
Juan Ozuna <jozuna@thecoursekey.com>
2022-06-20 07:43:01 -0400

    Created little grid view page

---
+++ project_1/lib/pages/grid_view_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:project_1/widgets/common_widget_container.dart';
+
+class GridViewPage extends StatelessWidget {
+  const GridViewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grid View'),
+      ),
+      body: _buildGridView(),
+    );
+  }
+
+  Widget _buildGridView() {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: List.generate(100, (index) {
+        return CommonWidgetContainer(
+          child: Text('$index'),
+        );
+      }),
+    );
+  }
+}
