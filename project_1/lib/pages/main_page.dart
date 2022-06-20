c097c1df2c1a4e6e6b98c42bc1f23f4103e4d657
Juan Ozuna <jozuna@thecoursekey.com>
2022-06-19 23:47:01 -0400

    Created common widget page

--- project_1/lib/pages/main_page.dart
+++ project_1/lib/pages/main_page.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:project_1/pages/common_widget_page.dart';
 import 'package:project_1/pages/stateful_page.dart';
-import 'package:project_1/widgets/primary_button.dart';
+
+class _ListItem {
+  final String title;
+  final Widget page;
+
+  _ListItem(this.title, this.page);
+}
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // This are the items to be shown
+    final items = <_ListItem>[
+      _ListItem('Stateful Widget', const StatefulPage()),
+      _ListItem('Common Widgets', const CommonWidgetPage()),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Main Page'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: PrimaryButton(
-              text: 'Go to Stateful Widget',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const StatefulPage(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      body: ListView(
+        children: items.asMap().entries.map((entry) {
+          final item = entry.value;
+          final index = entry.key;
+          return ListTile(
+            title: Text(item.title),
+            tileColor: index % 2 == 0 ? Colors.white : Colors.grey[200],
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => item.page,
+                ),
+              );
+            },
+          );
+        }).toList(),
       ),
     );
   }
