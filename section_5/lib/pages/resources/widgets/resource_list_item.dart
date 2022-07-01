a031347e03ca9c8c5bf6cc2de7c0df1677ea0304
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-01 10:01:26 -0400

    Some changes

--- section_5/lib/pages/resources/widgets/resource_list_item.dart
+++ section_5/lib/pages/resources/widgets/resource_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:section_5/core/color.dart';
 import 'package:section_5/models/resource_model.dart';
 
@@ -12,18 +13,39 @@ class ResourceListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: colorIndicator(),
-      title: Text(resource.name),
+    final color = HexColor(resource.color);
+    return Slidable(
+      endActionPane: ActionPane(
+        children: [
+          SlidableAction(
+            icon: Icons.edit,
+            onPressed: null,
+            label: 'Edit',
+            backgroundColor: Theme.of(context).errorColor,
+          ),
+          SlidableAction(
+            icon: Icons.delete,
+            onPressed: null,
+            label: 'Delete',
+            backgroundColor: Theme.of(context).primaryColor,
+          )
+        ],
+        motion: const ScrollMotion(),
+      ),
+      child: ListTile(
+        tileColor: color.withAlpha(60),
+        leading: colorIndicator(color),
+        title: Text(resource.name),
+      ),
     );
   }
 
-  Widget colorIndicator() {
+  Widget colorIndicator(Color color) {
     return Container(
-      width: 10,
-      height: 10,
+      width: 50,
+      height: 50,
       decoration: BoxDecoration(
-        color: HexColor(resource.color),
+        color: color,
         shape: BoxShape.circle,
       ),
     );
--- section_5/lib/theme/theme.dart
+++ section_5/lib/theme/theme.dart
@@ -3,13 +3,32 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Colors.pink;
   static const Color secondary = Colors.orange;
+  static const Color info = Colors.blue;
+  static const Color danger = Colors.red;
   static const Color white = Colors.white;
+  static Color black = Colors.grey.shade900;
+  static Color evenListItem = Colors.grey.shade300;
+  static Color oddListItem = Colors.grey.shade100;
 }
 
 class AppTheme {
   static ThemeData getTheme() {
     return ThemeData(
       primaryColor: AppColor.primary,
+      errorColor: AppColor.danger,
+      colorScheme: ColorScheme(
+        background: AppColor.white,
+        brightness: Brightness.light,
+        error: AppColor.danger,
+        primary: AppColor.primary,
+        secondary: AppColor.secondary,
+        onBackground: AppColor.black,
+        onError: AppColor.white,
+        onPrimary: AppColor.white,
+        onSecondary: AppColor.white,
+        onSurface: AppColor.black,
+        surface: AppColor.white,
+      ),
       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
         backgroundColor: AppColor.primary,
         selectedItemColor: AppColor.white,
