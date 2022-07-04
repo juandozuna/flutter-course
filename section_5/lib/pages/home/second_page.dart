71278b1289e5e01973ff15aa36aee570410cc17b
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-04 16:31:33 -0400

    Added image support

--- section_5/lib/pages/home/second_page.dart
+++ section_5/lib/pages/home/second_page.dart
@@ -1,11 +1,21 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:section_5/injector.dart';
 import 'package:section_5/providers/users_provider.dart';
 import 'package:section_5/widgets/primary_button.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
   static const String routeName = '/second';
   const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  Uint8List? image;
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +23,30 @@ class SecondPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Second Page'),
       ),
-      body: Center(
-          child: PrimaryButton(
-        label: 'Log Out',
-        onPressed: signOut,
-      )),
+      body: Column(
+        children: [
+          Center(
+              child: PrimaryButton(
+            label: 'Take Picture',
+            onPressed: signOut,
+          )),
+          if (image != null)
+            Expanded(
+              child: Image.memory(image!),
+            )
+        ],
+      ),
     );
   }
 
-  void signOut() {
-    final userProvider = get<UserProvider>();
-    userProvider.signOut();
+  void signOut() async {
+    final picker = ImagePicker();
+
+    final imagen = await picker.pickImage(source: ImageSource.camera);
+
+    if (imagen != null) {
+      final bytes = await imagen.readAsBytes();
+      setState(() => image = bytes);
+    }
   }
 }
