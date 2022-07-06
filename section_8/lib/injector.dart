f507f4dcb6a84850b812a1b6ee63fb56239f34d7
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-06 09:47:10 -0400

    Added missing dependencies

--- section_8/lib/injector.dart
+++ section_8/lib/injector.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
+import 'package:section_8/firebase_options.dart';
 
 final _injector = GetIt.instance;
 
@@ -11,10 +13,18 @@ Future<List<Provider<Object>>> initInjector() async {
   return _registerProviders();
 }
 
+Future<void> _registerAll() async {
+  await _initFirebase();
+}
+
+Future<void> _initFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
 List<Provider<Object>> _registerProviders() {
   return [];
 }
-
-Future<void> _registerAll() async {}
 
 T get<T>() => _injector<T>();
